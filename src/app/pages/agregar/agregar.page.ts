0f2920f1f0467498fef34d0d3f7b51b6107ab488
Atzayacatl2029 <atzayacatl10@gmail.com>
2019-12-17 23:02:29 -0600

    Avance de efectos de desliz y a punto de usar componentes diferentes

--- src/app/pages/agregar/agregar.page.ts
+++ src/app/pages/agregar/agregar.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DeseosService } from 'src/app/services/deseos.service';
 import { ActivatedRoute } from '@angular/router';
 import { Lista } from 'src/app/models/lista.model';
+import { ListaItem } from 'src/app/models/lista-item.model';
 
 @Component({
   selector: 'app-agregar',
@@ -11,16 +12,45 @@ import { Lista } from 'src/app/models/lista.model';
 export class AgregarPage implements OnInit {
 
   lista: Lista;
+  nombreItem: '';
 
   constructor( private deseosService: DeseosService, private route: ActivatedRoute ) {
     const listaId = this.route.snapshot.paramMap.get( 'listaId' );
     this.lista = this.deseosService.obtenerLista( listaId );
-
-    console.log(this.lista);
-
    }
 
   ngOnInit() {
   }
+
+  agregarItem(){
+    if( this.nombreItem.length === 0 ){
+      return;
+    }
+    const nuevoItem = new ListaItem( this.nombreItem );
+    this.lista.items.push( nuevoItem );
+
+    this.nombreItem = '';
+    this.deseosService.guardarStorage();
+  } 
+
+  cambioCheck( item: ListaItem ){
+    const pendientes = this.lista.items
+                      .filter( itemData => !itemData.completado)
+                      .length;
+    if( pendientes === 0 ){
+      this.lista.terminadaEnd = new Date();
+      this.lista.terminada = true;
+    } else {
+      this.lista.terminadaEnd = null;
+      this.lista.terminada = false;
+    }
+    this.deseosService.guardarStorage();
+  }
+
+  borrar( i: number ){
+    this.lista.items.splice( i, 1 );
+    this.deseosService.guardarStorage();
+  }
+
 
 }
--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DeseosService } from 'src/app/services/deseos.service';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { Lista } from 'src/app/models/lista.model';
 
 @Component({
   selector: 'app-tab1',
@@ -50,5 +51,11 @@ export class Tab1Page {
 
     alert.present();
   }
+
+  listaSeleccionada( lista: Lista ){
+    const listaId = lista.id;
+    this.router.navigateByUrl( `/tabs/tab1/agregar/${ listaId }` );
+  }
+
 
 }
